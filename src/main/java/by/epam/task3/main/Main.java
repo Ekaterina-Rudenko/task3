3ee91f94f7719cddd74c2f7567649dc31808828c
Ekaterina-Rudenko <katestasevich30@gmail.com>
2021-11-03 00:29:21 +0300

    Update Main.java

--- src/main/java/by/epam/task3/main/Main.java
+++ src/main/java/by/epam/task3/main/Main.java
@@ -1,34 +1,33 @@
 package by.epam.task3.main;
 
-import by.epam.task3.builder.DomGemBuilder;
-import by.epam.task3.builder.SaxGemBuilder;
-import by.epam.task3.builder.StaxGemBuilder;
+import by.epam.task3.builder.*;
 import by.epam.task3.exception.GemException;
 import by.epam.task3.validator.XMLValidator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.net.URL;
 
 public class Main {
+    static Logger logger = LogManager.getLogger();
     static final String FILE_XML_PATH = "data/gems.xml";
+    static final String TYPE_OF_PARSER_SAX = "Sax";
+    static final String TYPE_OF_PARSER_STAX = "Stax";
 
     public static void main(String[] args) throws GemException {
-        ClassLoader loader = Main.class.getClassLoader();
-        URL resource = loader.getResource(FILE_XML_PATH);
-        String filePath = new File(resource.getFile()).getAbsolutePath();
-        XMLValidator.validateXMLFile(filePath);
-
-       /* DomGemBuilder domBuilder = new DomGemBuilder();
-        domBuilder.buildGems(filePath);
-        System.out.println(domBuilder.getGems());
-
-
-        SaxGemBuilder saxBuilder = new SaxGemBuilder();
-        saxBuilder.buildGems(filePath);
-        System.out.println(saxBuilder.getGems());*/
-
-        StaxGemBuilder staxGemBuilder = new StaxGemBuilder();
-        staxGemBuilder.buildGems(filePath);
-        System.out.println(saxBuilder.getGems());
+        try {
+            ClassLoader loader = Main.class.getClassLoader();
+            URL resource = loader.getResource(FILE_XML_PATH);
+            String filePath = new File(resource.getFile()).getAbsolutePath();
+            XMLValidator.validateXMLFile(filePath);
+            AbstractGemBuilder builder = BuilderFactory.createGemsBuilder(TYPE_OF_PARSER_STAX);
+            builder.buildGems(filePath);
+            logger.info(builder.getGems());
+        } catch (GemException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
